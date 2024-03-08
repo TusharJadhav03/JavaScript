9e29be76c12c282723b952625ff02e6383282bf5
TUSHAR JADHAV <160405308+TusharJadhav03@users.noreply.github.com>
2024-03-08 17:25:00 +0530

    Add files via upload

---
+++ Image Slider with buttons (SetAttributes)/js/main.js
@@ -0,0 +1,30 @@
+function changeVal(){
+    getValue = document.getElementById("ran1").value;
+    document.getElementById("rangeVal").innerHTML = getValue;
+    document.getElementById("img1").setAttribute("width",getValue);
+}
+
+function openImg1(){
+    getValue = document.getElementById("ran1").value;
+    document.getElementById("rangeVal").innerHTML = getValue;
+    document.getElementById("img1").setAttribute("src","img2.jpg");
+}
+
+function openImg2(){
+    getValue = document.getElementById("ran1").value;
+    document.getElementById("rangeVal").innerHTML = getValue;
+    document.getElementById("img1").setAttribute("src","img3.jpg");
+}
+
+function openImg3(){
+    getValue = document.getElementById("ran1").value;
+    document.getElementById("rangeVal").innerHTML = getValue;
+    document.getElementById("img1").setAttribute("src","img4.jpg");
+}
+
+
+function openImg4(){
+    getValue = document.getElementById("ran1").value;
+    document.getElementById("rangeVal").innerHTML = getValue;
+    document.getElementById("img1").setAttribute("src","img5.jpg");
+}
